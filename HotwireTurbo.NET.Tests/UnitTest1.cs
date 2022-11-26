0f925e2bac835eea0c384263c1155991d76d9d1a
Michiel van der Meer <contact@michielvandermeer.com>
2022-11-26 18:02:15 +0100

    Cleanup

--- HotwireTurbo.NET.Tests/UnitTest1.cs
+++ HotwireTurbo.NET.Tests/UnitTest1.cs
@@ -1,11 +1,10 @@
-namespace HotwireTurbo.NET.Tests
+namespace HotwireTurbo.NET.Tests;
+
+[TestClass]
+public class UnitTest1
 {
-    [TestClass]
-    public class UnitTest1
+    [TestMethod]
+    public void TestMethod1()
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
     }
 }
